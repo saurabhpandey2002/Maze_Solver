6df2e05af1871b279e9f590c2a17cdd440f66989
Saurabh Pandey <156886681+saurabhpandey2002@users.noreply.github.com>
2024-01-17 13:22:54 +0530

    Add files via upload

---
+++ DepthFirstSearch.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.MAZE_SOLVER_PROJECTt;
+
+import java.util.List;
+
+/**
+ *
+ * @author birad
+ */
+public class DepthFirstSearch {
+   public static boolean searchpath(int [][] maze,int x,int y,List<Integer>path)
+   {
+      if(maze[y][x]==9)
+      {
+          path.add(x);
+          path.add(y);
+          return true;
+      }
+    if(maze[y][x]==0)
+    {
+        maze[y][x]=2;
+        int dx=-1;
+        int dy=0;
+        if(searchpath(maze,x+dx,y+dy,path))
+        {
+            path.add(x);
+            path.add(y);
+            return true;
+        }
+        dx=1;
+        dy=0;
+        if(searchpath(maze,x+dx,y+dy,path))
+        {
+            path.add(x);
+            path.add(y);
+            return true;
+        }
+        dx=0;
+        dy=-1;
+        if(searchpath(maze,x+dx,y+dy,path))
+        {
+            path.add(x);
+            path.add(y);
+            return true;
+        }
+         dx=0;
+        dy=1;
+        if(searchpath(maze,x+dx,y+dy,path))
+        {
+            path.add(x);
+            path.add(y);
+            return true;
+        }
+    }
+    return false;
+   }
+}
